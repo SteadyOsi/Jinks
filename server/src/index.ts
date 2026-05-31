44c3e3f2322cae2bdcc0968b4d2793ee3d82a135
SteadyOsi <Josiahstedman@gmail.com>
2026-05-31 21:42:42 +1000

    Refactor Code
    Refactored index into Types and routes. Using Router() as the way of doing this in express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,143 +1,19 @@
 import express from "express";
 import cors from "cors";
-import { get } from "node:http";
+import messageRouter from "./routes/messageRoutes";
 
 const app = express();
 
 const PORT = 3000;
 
-let nextId = 1;
-
 app.use(cors());
 app.use(express.json());
-
-type Message = {
-    id: number;
-    sender: string;
-    content: string;
-    timestamp: string;
-}
-
-const messages: Message[] = [];
 
 app.get("/", (req, res) => {
     res.send("server is running");
 });
 
-app.get("/messages", (req, res) => {
-    res.json(messages);
-});
-
-app.get("/messages/:id", (req, res) => {
-    // get id
-    const id = parseInt(req.params.id);
-
-    if(Number.isNaN(id)){
-        return res.status(400).json({error: "Bad Request"});
-    }
-
-    //find matching message 
-    const mes = messages.find(mes => mes.id === id);
-
-    //If found 
-    if(mes) {
-        res.json(mes);
-    } else { // otherwise
-            return res.status(404).json({error: "data not found"});
-    }
-    
-});
-
-app.get("/messages/:id", (req, res) => {
-    // get id
-    const id = parseInt(req.params.id);
-
-    if(Number.isNaN(id)){
-        return res.status(400).json({error: "Bad Request"});
-    }
-
-    //find matching message 
-    const mes = messages.find(mes => mes.id === id);
-
-    //If found 
-    if(mes) {
-        res.json(mes);
-    } else { // otherwise
-            return res.status(404).json({error: "data not found"});
-    }
-    
-});
-
-app.delete("/messages/:id", (req, res) => {
-    // get id
-    const id = parseInt(req.params.id);
-
-    if(Number.isNaN(id)){
-        return res.status(400).json({error: "Bad Request"});
-    }
-
-    //find matching message index
-    const mesIndex = messages.findIndex(mes => mes.id === id);
-
-    //If found 
-    if(mesIndex !== -1) {
-        messages.splice(mesIndex, 1);
-        res.status(204).send();
-    } else { // otherwise
-        return res.status(404).json({error: "data not found"});
-    }
-
-});
-
-app.post("/messages", (req, res) => {
-
-    if(typeof req.body.sender === "string" && typeof req.body.content === "string" && req.body.sender.trim() !== "" && req.body.content.trim() !== ""){
-        const newMessage: Message = {
-            id: nextId,
-            sender: req.body.sender,
-            content: req.body.content,
-            timestamp: new Date().toISOString()
-        };
-
-        nextId += 1;
-
-        messages.push(newMessage);
-
-        res.json({
-            success: true,
-            message: newMessage
-        });
-        
-    } else {
-        res.status(400).send("Ya fucked up");
-    }
-
-});
-
-app.patch("/messages/:id", (req, res) => {
-        // get id
-    const id = parseInt(req.params.id);
-
-    if(Number.isNaN(id)){
-        return res.status(400).json({error: "ID must be a valid number"});
-    }
-
-    // validating body: 
-    if(typeof req.body.content !== "string" || req.body.content.trim() === ""){
-        return res.status(400).json({error: "content must be a non-empty string"});
-    }
-
-    //find matching message index
-    const mesIndex = messages.findIndex(mes => mes.id === id);
-
-    //If found 
-    if(mesIndex !== -1) {
-        messages[mesIndex].content = req.body.content;
-        res.status(204).send();
-    } else { // otherwise
-        return res.status(404).json({error: "data not found"});
-    }
-});
+app.use("/messages/", messageRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
---
+++ server/src/types/Messages.ts
@@ -0,0 +1,6 @@
+export type Message = {
+    id: number;
+    sender: string;
+    content: string;
+    timestamp: string;
+}
