53d77cc069063e1f4696f350ed76b8b39d4df208
SteadyOsi <Josiahstedman@gmail.com>
2026-05-31 20:18:35 +1000

    patch update
    With patch being implemented, this is now a CRUD backend set up... aint that cool

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,8 @@ const app = express();
 
 const PORT = 3000;
 
+let nextId = 1;
+
 app.use(cors());
 app.use(express.json());
 
@@ -46,15 +48,58 @@ app.get("/messages/:id", (req, res) => {
     
 });
 
+app.get("/messages/:id", (req, res) => {
+    // get id
+    const id = parseInt(req.params.id);
+
+    if(Number.isNaN(id)){
+        return res.status(400).json({error: "Bad Request"});
+    }
+
+    //find matching message 
+    const mes = messages.find(mes => mes.id === id);
+
+    //If found 
+    if(mes) {
+        res.json(mes);
+    } else { // otherwise
+            return res.status(404).json({error: "data not found"});
+    }
+    
+});
+
+app.delete("/messages/:id", (req, res) => {
+    // get id
+    const id = parseInt(req.params.id);
+
+    if(Number.isNaN(id)){
+        return res.status(400).json({error: "Bad Request"});
+    }
+
+    //find matching message index
+    const mesIndex = messages.findIndex(mes => mes.id === id);
+
+    //If found 
+    if(mesIndex !== -1) {
+        messages.splice(mesIndex, 1);
+        res.status(204).send();
+    } else { // otherwise
+        return res.status(404).json({error: "data not found"});
+    }
+
+});
+
 app.post("/messages", (req, res) => {
 
     if(typeof req.body.sender === "string" && typeof req.body.content === "string" && req.body.sender.trim() !== "" && req.body.content.trim() !== ""){
         const newMessage: Message = {
-            id: messages.length + 1,
+            id: nextId,
             sender: req.body.sender,
             content: req.body.content,
             timestamp: new Date().toISOString()
         };
+
+        nextId += 1;
 
         messages.push(newMessage);
 
@@ -67,6 +112,31 @@ app.post("/messages", (req, res) => {
         res.status(400).send("Ya fucked up");
     }
 
+});
+
+app.patch("/messages/:id", (req, res) => {
+        // get id
+    const id = parseInt(req.params.id);
+
+    if(Number.isNaN(id)){
+        return res.status(400).json({error: "ID must be a valid number"});
+    }
+
+    // validating body: 
+    if(typeof req.body.content !== "string" || req.body.content.trim() === ""){
+        return res.status(400).json({error: "content must be a non-empty string"});
+    }
+
+    //find matching message index
+    const mesIndex = messages.findIndex(mes => mes.id === id);
+
+    //If found 
+    if(mesIndex !== -1) {
+        messages[mesIndex].content = req.body.content;
+        res.status(204).send();
+    } else { // otherwise
+        return res.status(404).json({error: "data not found"});
+    }
 });
 
 app.listen(PORT, () => {
