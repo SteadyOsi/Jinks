3c159f6b998f666849ef9e8e1dad352168c36205
SteadyOsi <Josiahstedman@gmail.com>
2026-06-17 08:40:47 +1000

    schema.ts
    Created the schema db table. will create the tables unless they already exist

---
+++ server/src/data_base/schema.ts
@@ -0,0 +1,24 @@
+import db from "./db";
+
+export function initaliseDbTables() {
+    const usersTable = `
+        CREATE TABLE IF NOT EXIST users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            password STRING NOT NULL
+        )
+    `;
+
+    const messageTable = `
+        CREATE TABLE IF NOT EXIST messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            senderID int,
+            contents TEXT,
+            time_stamp timestamp 
+        )
+    `;
+
+    db.exec(usersTable);
+    db.exec(messageTable);
+}
