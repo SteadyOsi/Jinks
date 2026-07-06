a239103f1fa97242eed4c57879e98f6eed71a415
SteadyOsi <Josiahstedman@gmail.com>
2026-07-07 08:34:11 +1000

    Conversations
    Started working on how group chats will be formed.

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import messageRouter from "./routes/messageRoutes";
 import userRouter from "./routes/userRoutes";
+import conversationRouter from "./routes/conversationsRoutes"
 import { initaliseDbTables } from "./database/schema";
 
 // creates DB tables if not already done
@@ -21,6 +22,8 @@ app.get("/", (req, res) => {
 app.use("/messages/", messageRouter);
 
 app.use("/user/", userRouter);
+
+app.use("/conversations/", conversationRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
