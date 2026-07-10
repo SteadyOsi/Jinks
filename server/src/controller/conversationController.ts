cb3ddb260b751459af8ceefca97059d3d4229f5b
SteadyOsi <Josiahstedman@gmail.com>
2026-07-11 09:31:34 +1000

    Update conversationController.ts

--- server/src/controller/conversationController.ts
+++ server/src/controller/conversationController.ts
@@ -3,91 +3,91 @@ import { ConvoInit, ConversationDetails } from "../types/Conversations";
 import { ConvoMember } from "../types/ConvoMember";
 import { createMemberRepo } from "../repositories/convoMembersRepository";
 import {
-        createConvoRepo,
-        getAllConvoRepo,
-        getConvoByIDRepo,
-        delConvoByIDRepo,
-        getConvoIDByTitleRepo,
-        getMessagesForConvoRepo,
+    createConvoRepo,
+    getAllConvoRepo,
+    getConvoByIDRepo,
+    delConvoByIDRepo,
+    getConvoIDByTitleRepo,
+    getMessagesForConvoRepo,
 } from "../repositories/conversationRepository";
 
 // get all of the Convos
 export function getAllConvo(req: Request, res: Response) {
-        return res.json(getAllConvoRepo());
+    return res.json(getAllConvoRepo());
 }
 
 // create a convo
 export function createConvo(req: Request, res: Response) {
-        // checking that the http inputs are valid
-        if (
-                typeof req.body.title === "string" &&
-                req.body.title.trim() !== "" &&
-                Array.isArray(req.body.memberIDs) &&
-                req.body.memberIDs.every((id: number) => typeof id === "number")
-        ) {
-                const newConvo: ConvoInit = {
-                        title: req.body.title,
-                        createdAT: new Date().toISOString(),
-                        members: req.body.memberIDs,
-                };
+    // checking that the http inputs are valid
+    if (
+        typeof req.body.title === "string" &&
+        req.body.title.trim() !== "" &&
+        Array.isArray(req.body.memberIDs) &&
+        req.body.memberIDs.every((id: number) => typeof id === "number")
+    ) {
+        const newConvo: ConvoInit = {
+            title: req.body.title,
+            createdAT: new Date().toISOString(),
+            members: req.body.memberIDs,
+        };
 
-                const createdConvoRes = createConvoRepo(newConvo);
-                console.log(getConvoIDByTitleRepo(newConvo.title));
-                console.log("debug");
+        const createdConvoRes = createConvoRepo(newConvo);
 
-                newConvo.members.forEach((userID) => {
-                        const newMember: ConvoMember = {
-                                conversationID: getConvoIDByTitleRepo(
-                                        newConvo.title,
-                                ),
-                                userID: userID,
-                        };
+        newConvo.members.forEach((userID) => {
+            const newMember: ConvoMember = {
+                conversationID: createdConvoRes.lastInsertRowid as number,
+                userID: userID,
+            };
 
-                        const result = createMemberRepo(newMember);
-                });
+            const result = createMemberRepo(newMember);
+        });
 
-                if (createdConvoRes) {
-                        return res.json({
-                                success: true,
-                                conversation: newConvo,
-                        });
-                } else {
-                        res.status(400).send("A internal error occured");
-                }
+        if (createdConvoRes) {
+            return res.json({
+                success: true,
+                conversation: newConvo,
+            });
+        } else {
+            res.status(400).send("A internal error occured");
         }
+    }
 }
 
 // get a convo by ID
 // and the messages of that convo
 export function getConvoByID(req: Request, res: Response) {
-        const convoID = parseInt(req.params.ID as string);
-        const convoMetaRes = getConvoByIDRepo(convoID);
-        const convoMesRes = getMessagesForConvoRepo(convoID); 
+    const convoID = parseInt(req.params.ID as string);
+    const convoMetaRes = getConvoByIDRepo(convoID);
 
-        console.log(convoMesRes);
+    if (!convoMetaRes) {
+        return res.status(404).json({ error: "Conversation not found" });
+    }
 
-        const chatWithMes: ConversationDetails = {
-            id: convoMetaRes.id,
-            title: convoMetaRes.title,
-            createdAT: convoMetaRes.createdAT,
-            messages: convoMesRes
-        }
+    const convoMesRes = getMessagesForConvoRepo(convoID);
 
-        if (chatWithMes) {
-                return res.json(chatWithMes);
-        } else {
-                return res.status(404).json({ error: "data not found" });
-        }
+    const chatWithMes: ConversationDetails = {
+        id: convoMetaRes.id,
+        title: convoMetaRes.title,
+        createdAT: convoMetaRes.createdAT,
+        messages: convoMesRes,
+    };
+
+    if (chatWithMes) {
+        return res.json(chatWithMes);
+    } else {
+        return res.status(404).json({ error: "data not found" });
+    }
 }
 
 // delete a convo by ID
+// should this delete the messages of the conversation?  this is something to keep in mind
 export function delConvoByID(req: Request, res: Response) {
-        const convoID = parseInt(req.params.ID as string);
-        const delConvoRes = delConvoByIDRepo(convoID);
+    const convoID = parseInt(req.params.ID as string);
+    const delConvoRes = delConvoByIDRepo(convoID);
 
-        if (delConvoRes.changes === 0) {
-                return res.status(404).json({ error: "data not found" });
-        } else {
-                return res.status(204).send();
-        }
+    if (delConvoRes.changes === 0) {
+        return res.status(404).json({ error: "data not found" });
+    } else {
+        return res.status(204).send();
+    }
 }
