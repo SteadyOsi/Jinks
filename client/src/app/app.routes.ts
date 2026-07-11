e5621bb8fef6204119c9bd718656b275de07450f
SteadyOsi <Josiahstedman@gmail.com>
2026-07-12 00:15:21 +1000

    Got the pages to work on the client.
    Not for the backend just got the routing to work

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Conversations } from './pages/conversations/conversations';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'conversations',
+    component: Conversations,
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+];
