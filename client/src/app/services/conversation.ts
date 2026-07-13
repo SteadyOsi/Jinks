4ceb4fcd4d6c2975d6f846b8f5b785c2f6760396
SteadyOsi <Josiahstedman@gmail.com>
2026-07-14 09:08:13 +1000

    created services

---
+++ client/src/app/services/conversation.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Conversation {
+  constructor(private http: HttpClient) {}  
+
+  getConversations() {
+    this.http.get()
+  }
+}
