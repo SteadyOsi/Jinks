0f3909c60b4f0b5bdec747d265e440fd409db695
SteadyOsi <Josiahstedman@gmail.com>
2026-07-14 12:29:17 +1000

    Update conversation.ts
    Have written the endpoint that this will call. Just need to call the function in the other components

--- client/src/app/services/conversation.ts
+++ client/src/app/services/conversation.ts
@@ -4,10 +4,10 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
 })
-export class Conversation {
+export class ConversationService {
   constructor(private http: HttpClient) {}  
 
   getConversations() {
-    return this.http.get("/conversations/");
+    return this.http.get("http://localhost:3000/conversations/");
   }
 }
