95d4f85e72db068f9358d5df0d18178ef6fdf200
SteadyOsi <Josiahstedman@gmail.com>
2026-07-21 08:37:27 +1000

    chats UI
    added some html for basic use and the ts to make it just usable

--- client/src/app/shared/conversation-preview/conversation-preview.ts
+++ client/src/app/shared/conversation-preview/conversation-preview.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ConversationWithMessages } from '../../models/conversationWithMessages';
 
 @Component({
   selector: 'app-conversation-preview',
   imports: [],
+  standalone: true,
   templateUrl: './conversation-preview.html',
   styleUrl: './conversation-preview.scss',
 })
-export class ConversationPreview {}
+export class ConversationPreview {
+  @Input()
+  chat!: ConversationWithMessages;
+}
+
