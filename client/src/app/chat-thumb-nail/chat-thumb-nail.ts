19e0c3b4bef5032cb046b3d243948af180e5c391
SteadyOsi <Josiahstedman@gmail.com>
2026-07-28 15:59:45 +1000

    .

---
+++ client/src/app/chat-thumb-nail/chat-thumb-nail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-thumb-nail',
+  imports: [],
+  templateUrl: './chat-thumb-nail.html',
+  styleUrl: './chat-thumb-nail.scss',
+})
+export class ChatThumbNail {}
--- client/src/app/layout/layout.ts
+++ client/src/app/layout/layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from '../sidebar/sidebar'
 import { ChatWindow } from '../chat-window/chat-window';
+import { ChatThumbNail } from '../chat-thumb-nail/chat-thumb-nail';
 
 @Component({
   selector: 'app-layout',
-  imports: [Sidebar, ChatWindow],
+  imports: [Sidebar, ChatWindow, ChatThumbNail],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
