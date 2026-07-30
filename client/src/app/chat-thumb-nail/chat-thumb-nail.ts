8b43e08e07c45b9da12401bb208df0bf7ccc7c9f
SteadyOsi <Josiahstedman@gmail.com>
2026-07-30 18:18:47 +1000

    layout now has prewview of messages

--- client/src/app/chat-thumb-nail/chat-thumb-nail.ts
+++ client/src/app/chat-thumb-nail/chat-thumb-nail.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 
+import { ConversationService } from '../services/conversation';
+import { ConversationPreviewMod } from '../models/conversationPreviewMod';
+import { ConversationPreview } from '../shared/conversation-preview/conversation-preview';
+
 @Component({
   selector: 'app-chat-thumb-nail',
-  imports: [],
+  imports: [ConversationPreview],
   templateUrl: './chat-thumb-nail.html',
   styleUrl: './chat-thumb-nail.scss',
 })
-export class ChatThumbNail {}
+export class ChatThumbNail {
+  constructor(private convo: ConversationService) {}
+  conversations: ConversationPreviewMod[] = [];
+
+  loadConversations() {
+    // change this to get a list of conversations with the last message.
+    this.convo.getConversationsPreview().subscribe((data) => {
+      this.conversations = data;
+      console.log(this.conversations);
+    });
+  }
+}
