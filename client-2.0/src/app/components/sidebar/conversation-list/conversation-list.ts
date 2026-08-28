7d6c8cb4d709f0ea3b57b567f9aa1b60dae13384
SteadyOsi <Josiahstedman@gmail.com>
2026-08-28 18:35:03 +1000

    Conversation preview works... Kinda

--- client-2.0/src/app/components/sidebar/conversation-list/conversation-list.ts
+++ client-2.0/src/app/components/sidebar/conversation-list/conversation-list.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ConversationPreview } from '../../../models/conversation';
+import { interval, Subscription } from 'rxjs';
+import { Conversation } from '../../../services/conversation';
+import { Chat } from "../../chat/chat";
 
 @Component({
   selector: 'app-conversation-list',
-  imports: [],
+  imports: [Chat],
   templateUrl: './conversation-list.html',
   styleUrl: './conversation-list.scss',
 })
-export class ConversationList {
+
+export class ConversationList implements OnInit, OnDestroy{
   
+  conversations: ConversationPreview[] = []; // holds a list of previews
+
+  private pollingSubscription?: Subscription;
+
+  constructor(private conversationService: Conversation) {}
+
+  ngOnInit() { // runs on 
+    this.pollingSubscription = interval(5000).subscribe(() => {
+      this.loadThumbNail();
+
+      console.log(this.conversations);
+    });
+  }
+
+  loadThumbNail() { // makes the call
+    return this.conversationService.getThumbNail().subscribe((data) => {
+      this.conversations = data;
+    });
+  }
+
+  ngOnDestroy() { // 
+    this.pollingSubscription?.unsubscribe();
+  }
 }
--- client-2.0/src/app/components/sidebar/sidebar.ts
+++ client-2.0/src/app/components/sidebar/sidebar.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ConversationList } from './conversation-list/conversation-list';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [ ConversationList],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
